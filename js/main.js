f7571dc13fe61f34f396871c87bf1a5f482897e5
adamlinc <adamlinc@gmail.com>
2019-10-29 20:10:27 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Adam Lincoln.", "Analista Power BI.", "Analista de Dados.", "Early Adopter."],
       typeSpeed: 100,
       loop: true,
     });
